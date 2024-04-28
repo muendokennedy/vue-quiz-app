25615932a701ac0cf16b0f92d20d59ca34f7cdf3
muendokennedy <kennedymunyao999@gmai.com>
2024-04-28 21:28:24 +0300

    Separated the card components into its own file and passed down data as through prop from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import questions from './data/quizes.json'
 import { ref, watch } from 'vue'
+import Card from './components/Card.vue'
 
 const quizes = ref(questions)
 const search = ref("")
@@ -18,13 +19,14 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="Math">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length}} questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -53,23 +55,5 @@ header input{
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-/* Card */
-.card{
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer
-}
-.card img{
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-.card .card-text h2{
-  font-weight: bold;
 }
 </style>
