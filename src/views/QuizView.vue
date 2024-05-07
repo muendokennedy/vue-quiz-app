decb950ec031bfa50b97d908e6f155d23f5db045
muendokennedy <kennedymunyao999@gmai.com>
2024-05-07 14:26:42 +0300

    Added the computed components or attributes which are questionStatus and barPercentage

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,7 +2,7 @@
 import Question from '../components/Question.vue'
 import QuizHeader from '../components/QuizHeader.vue';
 import { useRoute } from 'vue-router';
-import { ref } from 'vue';
+import { ref, watch, computed } from 'vue';
 import quizes from '../data/quizes.json';
 
 const route = useRoute()
@@ -13,14 +13,29 @@ const currentQuestionIndex = ref(0)
 
 const quiz = quizes.find(quiz => quiz.id === quizId)
 
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`);
+
+// watch( () => currentQuestionIndex.value, () => {
+//   questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+// })
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`
+})
+
+const barPercentage = computed(() => {
+  return `${currentQuestionIndex.value/quiz.questions.length * 100}%`
+})
+
 </script>
 
 <template>
   <div>
-    <QuizHeader/>
+    <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage"/>
       <div>
         <Question :question="quiz.questions[currentQuestionIndex]"/>
       </div>
+      <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
 
