cabfacf2a3cf218e44b60d4686767aea90f4c952
muendokennedy <kennedymunyao999@gmai.com>
2024-05-07 11:48:51 +0300

    Added the Question and QuizHeader component together with the QuizView to make the code blocks more readable

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import quizesView from '../views/QuizesView.vue'
+import quizView from '../views/QuizView.vue'
 
 const routes = [
   {
     path: '/',
     component: quizesView,
     name: 'quizes',
+  },
+  {
+    path: '/quiz/:id',
+    name: 'quiz',
+    component: quizView,
   }
 ]
 
---
+++ src/views/QuizView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Question from '../components/Question.vue'
+import QuizHeader from '../components/QuizHeader.vue';
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+import quizes from '../data/quizes.json';
+
+const route = useRoute()
+
+const quizId = parseInt(route.params.id)
+
+const currentQuestionIndex = ref(0)
+
+const quiz = quizes.find(quiz => quiz.id === quizId)
+
+</script>
+
+<template>
+  <div>
+    <QuizHeader/>
+      <div>
+        <Question :question="quiz.questions[currentQuestionIndex]"/>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
